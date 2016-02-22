c5cce1bb43e6836c3ed69a5297df104e68f059bf
Van-Duyet Le <lvduit08@gmail.com>
2016-02-23 02:42:33 +0700

    :memo:

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,25 +1,27 @@
-var Point = function(x, y) {
-}
+var Point = function(x, y) {}
 
-var Block = function(array_points, root_point, information) {
-	var block_info =  {
-		points: array_points, 
-		root_point: root_point,
-		information
-	};
+var Block = function(id, gateway_point, information) {
+    var block_info = {
+        points: array_points,
+        root_point: root_point,
+        information
+    };
 
-	function getPoints() {
-		return block_info.points;
-	}
+    function getPoints() {
+        return block_info.points;
+    }
 
-	return block_info;
+    return block_info;
 }
 
 /**
  * Define a namespace for the application.
  */
-window.app = {};
+window.app = window.app || {};
 var app = window.app;
+
+// Active debug
+app.debug = true;
 
 /**
  * @constructor
@@ -58,11 +60,125 @@ app.getBlockPoint = function(e) {
     var result = [];
 
     var raw = e.feature.G.geometry.A;
-    for (var i = 0; i < raw.length / 2 + 2; i+=2) {
+    for (var i = 0; i < raw.length / 2 + 2; i += 2) {
         var c = [];
         c.push(raw[i], raw[i + 1]);
-        result.push(c);        
+        result.push(c);
     }
 
     return result;
+}
+
+/**
+ * Get directions path, draw in maps
+ */
+app.getDirection = function(input) {
+    if (!input) return false;
+
+    var form_point = input.from.geoloc;
+    var to_point = input.to.geoloc;
+    var result = [];
+
+    if (!form_point || !to_point) return result;
+
+    console.log('start search ...');
+
+    var start_point_in_route = app.getGeoLoc(form_point);   
+    console.log('Gateway is: ', form_point, ' => ', start_point_in_route);
+
+    var results = [];
+
+    getRoute([], start_point_in_route);
+
+    function getRoute(route, point) {
+        var nexts = getNext(point);
+
+        for (var i in nexts) {
+            var next = nexts[i];
+
+            if (app.isNear(to_point, next)) {
+                route.push(next);
+                results.push(route);
+            } else {
+                route.push(next);
+                getRoute(route, next);
+            }
+        }
+    }
+
+    function getNext(point) {
+        var nexts = [];
+        for (var i in app.route) {
+            if (app.isNear(app.route[i].start, point)) 
+                nexts.push(app.route[i].next);
+        }
+
+        return nexts;
+    }
+}
+
+app.getGeoLoc = function(point) {
+    var min_value = 30.0;
+    var nearest_point = null;
+
+    point = point || [];
+
+    for (var i in this.route) {
+        var distance = this.distance(this.route[i].start, point);
+
+        if (distance < min_value && 
+                (nearest_point == null || 
+                    (nearest_point != null && nearest_point.distance > distance)
+                )
+            )
+            nearest_point = {
+                point: this.route[i],
+                distance: distance
+            };
+    }
+
+    return nearest_point;
+}
+
+app.distance = function(a, b) {
+    return Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
+}
+
+app.isNear = function(a, b) {
+    var d = this.distance(a, b);
+
+    return d < 10;
+}
+
+/**
+ * Array point to router tools 
+ */
+app.arrayPointToRouterGeneratorTools = function(data, is_reverse) {
+    var t = { start: [], next: [], points: [/* [x, y] */], length: 0 };
+    var t_data = JSON.parse(JSON.stringify(data));
+    t.start = t_data.shift();
+    t.next = t_data.pop();
+    t.points = t_data;
+
+    // Calc length 
+    var l = 0;
+    var data_length = data.length;
+
+    for (var i = 0; i < data_length; i++) {
+        var a = data[i];
+        var b = data[i + 1];
+        if (a && b) {
+            l += Math.sqrt(Math.pow(a[0] - b[0], 2) + Math.pow(a[1] - b[1], 2));
+        }
+    }    
+    t.length = l;
+
+    console.info('window.app.route.push(' + JSON.stringify(t) + '); ' + (is_reverse ? '/* reverse */': ''));
+    if (is_reverse) return;
+
+    var t2_data = [];
+    for (var i = data_length - 1; i >= 0; i--) {
+        if (data[i]) t2_data.push(data[i]);
+    }
+    app.arrayPointToRouterGeneratorTools(t2_data, true);   
 }
---
+++ assets/js/route.js
@@ -0,0 +1,106 @@
+window.app = window.app || {};
+window.app.route = []
+// { name: '', point:[], next: [], length: 0 },
+
+window.app.route.push({"start":[143.875,787.375],"next":[156.625,770.125],"points":[],"length":21.450524469112636}); 
+window.app.route.push({"start":[156.625,770.125],"next":[143.875,787.375],"points":[],"length":21.450524469112636}); /* reverse */
+window.app.route.push({"start":[157.375,769.375],"next":[190.375,754.875],"points":[[165.375,761.875],[174.625,757.875]],"length":37.076847029927805}); 
+window.app.route.push({"start":[190.375,754.875],"next":[157.375,769.375],"points":[[174.625,757.875],[165.375,761.875]],"length":37.076847029927805}); /* reverse */
+window.app.route.push({"start":[230.125,747.875],"next":[232.125,791.875],"points":[[240.625,772.875],[240.625,781.625],[237.375,788.125]],"length":49.58445860306879}); 
+window.app.route.push({"start":[232.125,791.875],"next":[230.125,747.875],"points":[[237.375,788.125],[240.625,781.625],[240.625,772.875]],"length":49.58445860306878}); /* reverse */
+
+window.app.route.push({"start":[230.875,747.125],"next":[296.875,734.875],"points":[[230.875,747.125]],"length":67.12721132297989}); 
+window.app.route.push({"start":[296.875,734.875],"next":[230.875,747.125],"points":[[230.875,747.125]],"length":67.12721132297989}); /* reverse */
+window.app.route.push({"start":[231.375,747.375],"next":[225.625,796.875],"points":[[240.375,770.875],[239.625,784.125],[234.375,792.375]],"length":58.05380643919142}); 
+window.app.route.push({"start":[225.625,796.875],"next":[231.375,747.375],"points":[[234.375,792.375],[239.625,784.125],[240.375,770.875]],"length":58.05380643919142}); /* reverse */
+window.app.route.push({"start":[202.875,822.125],"next":[258.375,846.375],"points":[],"length":60.56659557875116}); 
+window.app.route.push({"start":[258.375,846.375],"next":[202.875,822.125],"points":[],"length":60.56659557875116}); /* reverse */
+window.app.route.push({"start":[259.125,846.625],"next":[311.625,773.125],"points":[[296.375,805.625],[304.625,796.125],[309.625,783.375]],"length":92.11548188230711}); 
+window.app.route.push({"start":[311.625,773.125],"next":[259.125,846.625],"points":[[309.625,783.375],[304.625,796.125],[296.375,805.625]],"length":92.11548188230712}); /* reverse */
+window.app.route.push({"start":[298.875,733.875],"next":[344.625,711.875],"points":[[315.125,728.875],[330.875,723.125]],"length":51.534458777466384}); 
+window.app.route.push({"start":[344.625,711.875],"next":[298.875,733.875],"points":[[330.875,723.125],[315.125,728.875]],"length":51.534458777466384}); /* reverse */
+window.app.route.push({"start":[427.625,657.125],"next":[456.375,678.125],"points":[],"length":35.602843987524366}); 
+window.app.route.push({"start":[456.375,678.125],"next":[427.625,657.125],"points":[],"length":35.602843987524366}); /* reverse */
+window.app.route.push({"start":[456.875,678.125],"next":[460.375,681.875],"points":[],"length":5.129571132170798}); 
+window.app.route.push({"start":[460.375,681.875],"next":[456.875,678.125],"points":[],"length":5.129571132170798}); /* reverse */
+window.app.route.push({"start":[460.625,682.125],"next":[479.875,669.125],"points":[],"length":23.228484668613234}); 
+window.app.route.push({"start":[479.875,669.125],"next":[460.625,682.125],"points":[],"length":23.228484668613234}); /* reverse */
+window.app.route.push({"start":[457.125,678.375],"next":[478.625,666.625],"points":[[474.125,666.125]],"length":25.48151083686897}); 
+window.app.route.push({"start":[478.625,666.625],"next":[457.125,678.375],"points":[[474.125,666.125]],"length":25.48151083686897}); /* reverse */
+window.app.route.push({"start":[481.125,667.875],"next":[497.375,673.125],"points":[[486.375,670.375]],"length":17.153392145255072}); 
+window.app.route.push({"start":[497.375,673.125],"next":[481.125,667.875],"points":[[486.375,670.375]],"length":17.153392145255072}); /* reverse */
+window.app.route.push({"start":[500.875,674.625],"next":[534.625,701.625],"points":[],"length":43.22108860267173}); 
+window.app.route.push({"start":[534.625,701.625],"next":[500.875,674.625],"points":[],"length":43.22108860267173}); /* reverse */
+
+
+// =============
+window.app.route.push({"start":[143.125,788.375],"next":[167.625,760.125],"points":[[156.875,770.625]],"length":37.47978704646643}); 
+window.app.route.push({"start":[167.625,760.125],"next":[143.125,788.375],"points":[[156.875,770.625]],"length":37.47978704646643}); /* reverse */
+window.app.route.push({"start":[816.75,429.75],"next":[793.875,406.375],"points":[[803.125,420.375]],"length":33.31860437931558}); 
+window.app.route.push({"start":[793.875,406.375],"next":[816.75,429.75],"points":[[803.125,420.375]],"length":33.31860437931558}); /* reverse */
+window.app.route.push({"start":[792.875,406.625],"next":[764.125,425.375],"points":[],"length":34.32382554436495}); 
+window.app.route.push({"start":[764.125,425.375],"next":[792.875,406.625],"points":[],"length":34.32382554436495}); /* reverse */
+window.app.route.push({"start":[763.375,425.625],"next":[722.125,452.625],"points":[],"length":49.300735288634385}); 
+window.app.route.push({"start":[722.125,452.625],"next":[763.375,425.625],"points":[],"length":49.300735288634385}); /* reverse */
+window.app.route.push({"start":[721.875,452.875],"next":[703.875,440.125],"points":[],"length":22.058161754779114}); 
+window.app.route.push({"start":[703.875,440.125],"next":[721.875,452.875],"points":[],"length":22.058161754779114}); /* reverse */
+window.app.route.push({"start":[703.375,439.125],"next":[676.125,417.625],"points":[],"length":34.71040910159372}); 
+window.app.route.push({"start":[676.125,417.625],"next":[703.375,439.125],"points":[],"length":34.71040910159372}); /* reverse */
+window.app.route.push({"start":[676.375,417.125],"next":[636.375,388.375],"points":[],"length":49.26015123809508}); 
+window.app.route.push({"start":[636.375,388.375],"next":[676.375,417.125],"points":[],"length":49.26015123809508}); /* reverse */
+window.app.route.push({"start":[636.875,388.125],"next":[611.875,370.875],"points":[],"length":30.373713964545068}); 
+window.app.route.push({"start":[611.875,370.875],"next":[636.875,388.125],"points":[],"length":30.373713964545068}); /* reverse */
+window.app.route.push({"start":[611.875,371.125],"next":[644.375,344.375],"points":[],"length":42.09290320232141}); 
+window.app.route.push({"start":[644.375,344.375],"next":[611.875,371.125],"points":[],"length":42.09290320232141}); /* reverse */
+window.app.route.push({"start":[644.125,344.875],"next":[665.875,354.125],"points":[[653.875,351.125]],"length":23.95055169875345}); 
+window.app.route.push({"start":[665.875,354.125],"next":[644.125,344.875],"points":[[653.875,351.125]],"length":23.95055169875345}); /* reverse */
+window.app.route.push({"start":[684.125,357.125],"next":[736.125,379.125],"points":[[711.125,362.625],[726.625,369.875]],"length":57.925687031733425}); 
+window.app.route.push({"start":[736.125,379.125],"next":[684.125,357.125],"points":[[726.625,369.875],[711.125,362.625]],"length":57.92568703173343}); /* reverse */
+window.app.route.push({"start":[743.875,398.125],"next":[754.125,418.875],"points":[[745.625,409.375]],"length":24.13284632071766}); 
+window.app.route.push({"start":[754.125,418.875],"next":[743.875,398.125],"points":[[745.625,409.375]],"length":24.13284632071766}); /* reverse */
+window.app.route.push({"start":[742.625,387.875],"next":[744.875,398.875],"points":[],"length":11.22775578644281}); 
+window.app.route.push({"start":[744.875,398.875],"next":[742.625,387.875],"points":[],"length":11.22775578644281}); /* reverse */
+window.app.route.push({"start":[712.125,362.625],"next":[747.875,363.375],"points":[[712.125,362.625],[730.625,364.625]],"length":35.90302462235637}); 
+window.app.route.push({"start":[747.875,363.375],"next":[712.125,362.625],"points":[[730.625,364.625],[712.125,362.625]],"length":35.90302462235637}); /* reverse */
+window.app.route.push({"start":[776.375,361.375],"next":[776.375,360.625],"points":[],"length":0.75}); 
+window.app.route.push({"start":[776.375,360.625],"next":[776.375,361.375],"points":[],"length":0.75}); /* reverse */
+window.app.route.push({"start":[776.125,360.125],"next":[799.125,348.125],"points":[[787.625,356.875]],"length":26.400764419335545}); 
+window.app.route.push({"start":[799.125,348.125],"next":[776.125,360.125],"points":[[787.625,356.875]],"length":26.400764419335545}); /* reverse */
+window.app.route.push({"start":[792.625,405.875],"next":[780.875,377.125],"points":[[792.625,405.875],[786.375,397.625],[782.125,387.625]],"length":31.78992066957089}); 
+window.app.route.push({"start":[780.875,377.125],"next":[792.625,405.875],"points":[[782.125,387.625],[786.375,397.625],[792.625,405.875]],"length":31.78992066957089}); /* reverse */
+window.app.route.push({"start":[611.875,369.875],"next":[560.125,391.375],"points":[],"length":56.038491235935325}); 
+window.app.route.push({"start":[560.125,391.375],"next":[611.875,369.875],"points":[],"length":56.038491235935325}); /* reverse */
+window.app.route.push({"start":[560.375,391.375],"next":[596.125,429.625],"points":[[584.625,418.625]],"length":52.39156339678159}); 
+window.app.route.push({"start":[596.125,429.625],"next":[560.375,391.375],"points":[[584.625,418.625]],"length":52.39156339678159}); /* reverse */
+window.app.route.push({"start":[614.125,444.875],"next":[668.875,488.375],"points":[],"length":69.92719428090905}); 
+window.app.route.push({"start":[668.875,488.375],"next":[614.125,444.875],"points":[],"length":69.92719428090905}); /* reverse */
+window.app.route.push({"start":[669.625,489.375],"next":[722.375,452.875],"points":[],"length":64.14680428517075}); 
+window.app.route.push({"start":[722.375,452.875],"next":[669.625,489.375],"points":[],"length":64.14680428517075}); /* reverse */
+window.app.route.push({"start":[723.375,453.625],"next":[749.375,474.375],"points":[[738.375,465.375]],"length":33.266869415843125}); 
+window.app.route.push({"start":[749.375,474.375],"next":[723.375,453.625],"points":[[738.375,465.375]],"length":33.266869415843125}); /* reverse */
+window.app.route.push({"start":[754.875,489.375],"next":[759.625,508.125],"points":[],"length":19.3423111338847}); 
+window.app.route.push({"start":[759.625,508.125],"next":[754.875,489.375],"points":[],"length":19.3423111338847}); /* reverse */
+window.app.route.push({"start":[759.875,508.375],"next":[811.875,489.125],"points":[[775.125,505.125],[791.125,499.875],[805.875,494.875]],"length":56.31658344503144}); 
+window.app.route.push({"start":[811.875,489.125],"next":[759.875,508.375],"points":[[805.875,494.875],[791.125,499.875],[775.125,505.125]],"length":56.31658344503144}); /* reverse */
+window.app.route.push({"start":[791.875,446.875],"next":[763.375,425.125],"points":[],"length":35.85125520815136}); 
+window.app.route.push({"start":[763.375,425.125],"next":[791.875,446.875],"points":[],"length":35.85125520815136}); /* reverse */
+window.app.route.push({"start":[816.875,428.375],"next":[836.875,465.625],"points":[[829.375,440.625],[837.125,452.125]],"length":44.87177848793658}); 
+window.app.route.push({"start":[836.875,465.625],"next":[816.875,428.375],"points":[[837.125,452.125],[829.375,440.625]],"length":44.87177848793658}); /* reverse */
+window.app.route.push({"start":[646.125,343.625],"next":[641.625,336.625],"points":[[651.125,337.875]],"length":17.201759275432487}); 
+window.app.route.push({"start":[641.625,336.625],"next":[646.125,343.625],"points":[[651.125,337.875]],"length":17.201759275432487}); /* reverse */
+window.app.route.push({"start":[643.625,344.875],"next":[622.375,328.375],"points":[[631.375,335.875]],"length":26.916114627420242}); 
+window.app.route.push({"start":[622.375,328.375],"next":[643.625,344.875],"points":[[631.375,335.875]],"length":26.916114627420242}); /* reverse */
+window.app.route.push({"start":[607.625,318.875],"next":[576.625,311.375],"points":[[591.625,314.375]],"length":31.917828679373017}); 
+window.app.route.push({"start":[576.625,311.375],"next":[607.625,318.875],"points":[[591.625,314.375]],"length":31.917828679373017}); /* reverse */
+window.app.route.push({"start":[825.625,341.75],"next":[943.625,300.5],"points":[[849.625,339],[875.375,336.75],[899.125,327.75],[923.875,315]],"length":127.74557526602344}); 
+window.app.route.push({"start":[943.625,300.5],"next":[825.625,341.75],"points":[[923.875,315],[899.125,327.75],[875.375,336.75],[849.625,339]],"length":127.74557526602344}); /* reverse */
+window.app.route.push({"start":[996.125,291],"next":[958.875,348.25],"points":[],"length":68.30172032972523}); 
+window.app.route.push({"start":[958.875,348.25],"next":[996.125,291],"points":[],"length":68.30172032972523}); /* reverse */
+window.app.route.push({"start":[959.625,348.25],"next":[941.375,378.25],"points":[],"length":35.11498967677479}); 
+window.app.route.push({"start":[941.375,378.25],"next":[959.625,348.25],"points":[],"length":35.11498967677479}); /* reverse */
+window.app.route.push({"start":[941.375,378.25],"next":[872.625,379],"points":[[928.125,376.5],[903.875,377]],"length":68.93415509394912}); 
+window.app.route.push({"start":[872.625,379],"next":[941.375,378.25],"points":[[903.875,377],[928.125,376.5]],"length":68.93415509394912}); /* reverse */
+window.app.route.push({"start":[839.625,386.5],"next":[793.625,406],"points":[[810.875,396.25]],"length":50.173044510162114}); 
+window.app.route.push({"start":[793.625,406],"next":[839.625,386.5],"points":[[810.875,396.25]],"length":50.173044510162114}); /* reverse */
+window.app.route.push({"start":[942.25,379.5],"next":[900.25,448.5],"points":[[942.25,379.5],[918.25,417]],"length":80.80262610631095}); 
+window.app.route.push({"start":[900.25,448.5],"next":[942.25,379.5],"points":[[918.25,417],[942.25,379.5]],"length":80.80262610631095}); /* reverse */
